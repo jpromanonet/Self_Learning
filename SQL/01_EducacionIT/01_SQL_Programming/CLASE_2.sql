cd02fb582b512a21ede7fca252034d236c715db5
Juan P. Romano <35672464+jpromanonet@users.noreply.github.com>
2018-11-15 19:56:16 -0300

    Add files via upload

--- SQL/01_EducacionIT/01_SQL_Programming/CLASE_2.sql
+++ SQL/01_EducacionIT/01_SQL_Programming/CLASE_2.sql
@@ -19,4 +19,23 @@ INSERT INTO Personas(Nombre, Apellido, Fecha_Nacimiento)
 
 /** EJERCICIO 3 | Agregar un campo a la tabla **/
 
-ALTER TABLE Personas ADD DNI INT 
+ALTER TABLE Personas ADD DNI INT
+
+CREATE TABLE  -- TRANSACT SQL
+	Personas2 
+		(id INT IDENTITY,
+		Nombre CHAR(50) NOT NULL,
+		Apellido VARCHAR(50),
+		Fecha_Nacimiento DATE NOT NULL)
+
+INSERT INTO Personas2(Nombre, Apellido, Fecha_Nacimiento)
+	VALUES ('Jose','Gomez','1950-06-05')
+
+SELECT *
+	FROM Personas2
+
+ALTER TABLE Personas2 ADD Sexo CHAR(1)
+
+UPDATE Personas2 
+	SET Sexo = 'M'
+	WHERE Nombre = 'Jose' 
