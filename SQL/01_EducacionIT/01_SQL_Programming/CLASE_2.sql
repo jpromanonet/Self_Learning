dba549886e8054d295c09ef78fb8fe58b5e3835d
Juan P. Romano <35672464+jpromanonet@users.noreply.github.com>
2018-11-15 19:46:26 -0300

    Add files via upload

--- SQL/01_EducacionIT/01_SQL_Programming/CLASE_2.sql
+++ SQL/01_EducacionIT/01_SQL_Programming/CLASE_2.sql
@@ -1,11 +1,22 @@
- -- EJERCICIO 1 | Creacion tabla.
+/** EJERCICIO 1 | Creacion tabla. **/
  
- CREATE TABLE 
+ CREATE TABLE  -- TRANSACT SQL
 		Personas 
 			(id INT IDENTITY,
 			Nombre CHAR(50),
 			Apellido VARCHAR(50),
 			Fecha_Nacimiento DATE)
 
-SELECT *
+SELECT * -- SQL ANSI
 	FROM Personas
+
+/** EJERCICIO 2 | INSERT INTO **/
+
+INSERT INTO Personas(Nombre, Apellido, Fecha_Nacimiento)
+	VALUES ('Jose','Gomez','1950-06-05')
+
+-- SET IDENTITY_INSERT Personas OFF
+
+/** EJERCICIO 3 | Agregar un campo a la tabla **/
+
+ALTER TABLE Personas ADD DNI INT 
