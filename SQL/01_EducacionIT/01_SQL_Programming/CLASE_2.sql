b73d5e20cee31795d80d8918eb971ff118b694dd
Juan P. Romano <35672464+jpromanonet@users.noreply.github.com>
2018-11-15 19:29:46 -0300

    Add files via upload

---
+++ SQL/01_EducacionIT/01_SQL_Programming/CLASE_2.sql
@@ -0,0 +1,11 @@
+ -- EJERCICIO 1 | Creacion tabla.
+ 
+ CREATE TABLE 
+		Personas 
+			(id INT IDENTITY,
+			Nombre CHAR(50),
+			Apellido VARCHAR(50),
+			Fecha_Nacimiento DATE)
+
+SELECT *
+	FROM Personas
