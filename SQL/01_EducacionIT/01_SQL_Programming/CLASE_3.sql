23ebe7f3d9cc43ef2e3383626939359bfe91ec36
Juan P. Romano <35672464+jpromanonet@users.noreply.github.com>
2018-11-20 19:30:12 -0300

    Add files via upload

--- SQL/01_EducacionIT/01_SQL_Programming/CLASE_3.sql
+++ SQL/01_EducacionIT/01_SQL_Programming/CLASE_3.sql
@@ -2,4 +2,14 @@
 
 SELECT *
 	FROM HumanResources.Employee 
-		WHERE 
+		WHERE JobTitle LIKE '%Manager' -- Culmina con manager.
+
+SELECT CONVERT(DATE, BirthDate, 112), * -- Este excluye los valores.
+	FROM HumanResources.Employee
+		WHERE CONVERT(DATE, BirthDate, 112) >= '1950-01-01'
+			AND CONVERT(DATE, BirthDate, 112) <= '1950-01-01'
+
+			SELECT CONVERT(DATE, BirthDate, 112), * -- Este excluj
+	FROM HumanResources.Employee
+		WHERE CONVERT(DATE, BirthDate, 112) BETWEEN '1950-01-01'
+			AND '1950-01-01'
