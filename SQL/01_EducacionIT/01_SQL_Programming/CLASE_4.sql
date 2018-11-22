e8b56e360b99f7a2777e5c2737446e59511c10ae
Juan P. Romano <35672464+jpromanonet@users.noreply.github.com>
2018-11-22 19:59:43 -0300

    Add files via upload

--- SQL/01_EducacionIT/01_SQL_Programming/CLASE_4.sql
+++ SQL/01_EducacionIT/01_SQL_Programming/CLASE_4.sql
@@ -23,7 +23,8 @@ SELECT color,
 			FROM Production.Product PP
 				WHERE color IS NOT NULL
 
-SELECT 'Gama de Colores' =
+SELECT color,
+	'Gama_de_Colores' =
 		CASE
 			WHEN color in('Black', 'White') 
 				THEN 'Puros'
@@ -45,4 +46,37 @@ SELECT 'Gama de Colores' =
 		COUNT (*) AS Cantidad
 			FROM Production.Product PP
 				WHERE color IS NOT NULL
-					GROUP BY color
+					GROUP BY Color
+
+INSERT color,
+	'Gama_de_Colores' =
+		CASE
+			WHEN color in('Black', 'White') 
+				THEN 'Puros'
+			WHEN (color = 'Silver'
+					OR color = 'Grey' 
+					OR color = 'Silver/Black') 
+				THEN 'Escala de grises'
+			WHEN color = 'Multi'
+				THEN 'Paleta'	
+			WHEN color = 'Blue' 
+				THEN 'RGB'
+			WHEN color = 'Red' 
+				THEN 'RGB'
+			WHEN color = 'Yellow' 
+			THEN 'RGB'
+		ELSE 
+			'Desconocido'
+		END,
+		COUNT (*) AS Cantidad
+			FROM Production.Product PP
+				WHERE color IS NOT NULL
+					GROUP BY Color
+						INTO #TablaColor
+
+-- NOTA: SIEMPRE resuelve las operaciones logicas del 'FROM' hacia abajo y luego resuelve las operaciones del dataset en 'SELECT'
+
+/** Ejercicio 2 | INDEX **/
+
+CREATE INDEX IDX_PRODUCT_PRODUCT_NUMBER
+	ON Production.Product (ProductNumber)
