b1cac1ef8f19fd4ab86299aaac9310136c65eb14
Juan P. Romano <35672464+jpromanonet@users.noreply.github.com>
2018-12-04 15:34:12 -0300

    Boolean loop

---
+++ Python/02_Exercises/py_exec_09.py
@@ -0,0 +1,17 @@
+print('Another silly program')
+
+print('Magic Word?') # Asking for the magic word
+
+magic_word = input()
+
+if magic_word == 'Stan': # Comparing the magic word
+    print('Access Granted') 
+else:
+    wrong_Magic_Word_Counter = 1 # This variable starts the loop for wrong answers
+    # Starting the loop
+    while True: # Infinite loop
+        print('ah ah ah')
+        if wrong_Magic_Word_Counter == 20:
+            break # Breakpoint at 18 times of repeat.
+        wrong_Magic_Word_Counter = wrong_Magic_Word_Counter + 1
+    print('The T-Rex ate you! HA!')
