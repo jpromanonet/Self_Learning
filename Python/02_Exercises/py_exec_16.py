c3c29dad47fdd8444423de56008df10dce3d6155
Juan P. Romano <35672464+jpromanonet@users.noreply.github.com>
2018-12-05 15:25:35 -0300

    More python stuff

---
+++ Python/02_Exercises/py_exec_16.py
@@ -0,0 +1,13 @@
+def hello_function():
+    print('H')
+    print('He')
+    print('Hel')
+    print('Hell')
+    print('Hello-a!')
+
+def names(name):
+    print('Your name is ' + name)
+
+hello_function()
+
+names('Jack')
