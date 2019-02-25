f7d3e3af73bf8a0af2ffca53f3e2273dd6e20e05
Juan P. Romano <35672464+jpromanonet@users.noreply.github.com>
2019-02-25 15:10:59 -0300

    JS Exercises
    Simple exercises with JS

---
+++ 01_FreeCodeCamp/01_Practices/JS_Exercises.js
@@ -0,0 +1,47 @@
+// 1st Exercise of the 60th day!
+
+
+// Declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
+
+// 2nd Exercise of the 60th day!
+
+var sum = 10 + 10;
+
+// 3rd Exercise of the 60th day!
+
+var product = 8 * 10;
+
+// 4th Exercise of the 60th day!
+
+var quotient = 66 / 33;
+
+// 5th Exercise of the 60th day!
+
+var myVar = 87;
+
+	// Only change code below this line
+myVar++;
+
+// 6th Exercise of the 60th day!
+
+var myVar = 11;
+
+	// Only change code below this line
+myVar--;
+
+// 7th Exercise of the 60th day!
+
+var ourDecimal = 5.7;
+
+	// Only change code below this line
+
+var myDecimal = 5.7;
+
