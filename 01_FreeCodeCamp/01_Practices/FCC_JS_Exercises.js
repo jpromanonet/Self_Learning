3b22a5b5c5014c7088f97cbfec16cc9501e82917
Juan P. Romano <35672464+jpromanonet@users.noreply.github.com>
2019-03-18 15:17:06 -0300

    More JS exercises

---
+++ 01_FreeCodeCamp/01_Practices/FCC_JS_Exercises.js
@@ -0,0 +1,25 @@
+// Exercise 1 | 18/03/2019
+
+    // Example
+    var ourArray = [1,2,3];
+    var removedFromOurArray = ourArray.pop(); 
+    // removedFromOurArray now equals 3, and ourArray now equals [1,2]
+
+    // Setup
+    var myArray = [["John", 23], ["cat", 2]];
+
+    // Only change code below this line.
+    var removedFromMyArray = myArray.pop();
+
+// Exercise 2 | 18/03/2019
+
+    // Example
+    var ourArray = [1,2,3];
+    var removedFromOurArray = ourArray.pop(); 
+    // removedFromOurArray now equals 3, and ourArray now equals [1,2]
+
+    // Setup
+    var myArray = [["John", 23], ["cat", 2]];
+
+    // Only change code below this line.
+    var removedFromMyArray = myArray.shift();
